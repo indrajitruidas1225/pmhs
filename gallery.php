994efc32227599558f2705e489b9ec1ec8d55254
Indrajit Ruidas <86678121+indrajitruidas1225@users.noreply.github.com>
2023-03-01 15:22:53 +0530

    Add files via upload

--- gallery.php
+++ gallery.php
@@ -1,18 +1,23 @@
 <?php 
     include "includes/navbar.php";
 ?>
-
+<style>
+.box {
+    transition: box-shadow .3s;
+    border-radius:10px;
+}
+.box:hover {
+  box-shadow: 0 0 11px rgba(33,33,33,.8); 
+}
+</style>
 <?php
 $files = scandir('gallery');
 echo "<div class='container'>";
 echo "<div style='background-color:#6B5B95;'><h4 style='font-family:Times' class='mt-2 mb-2 ml-2 text-white'>PMHS Gallery</h4></div>";
 echo "<marquee><span style='color:red'>Click on image for fullscreen view</span></marquee>";
-// echo "<div class='row'>";
 foreach ($files as $file) {
   if ($file !== "." && $file !== "..") {
-    // Give Image source -- src='folder-name/$file'
-    // echo "<div class='col-6 col-md-4 mt-3 mb-3'>
-    //      <a href='gallery/$file'> <img src='gallery/$file' alt='image' width='100%' height='200px'/></a></div>";
+   
     echo "<div class='card mt-4 mb-4 border-info'>";
     echo "<h4 style='font-family:Verdana' class='text-center mt-3'>$file</h4>";
     echo "<div class='card-body'>";
@@ -20,12 +25,11 @@ foreach ($files as $file) {
     echo "<div class='row'>";
     foreach($photos as $photo){
         if($photo!=="." && $photo!==".."){
-            echo "<div class='col-6 col-md-3 mt-2'>";
+            echo "<div class='col-6 col-md-3 mt-2 box'>";
             echo "<a href='gallery/$file/$photo'> <img src='gallery/$file/$photo' alt='image' width='100%' height='150px'/></a>";
             echo "</div>";
         }
     }
-    //  echo "</div>";
     echo "</div>";
     echo "</div>";
     echo "</div>";
